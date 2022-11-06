f6e8883ed05c4577cf4d7e9f75430737cd3f388a
Felix <barosiofelix@gmail.com>
2022-11-06 10:13:04 +0300

    get single receipe

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,18 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
+
+  #Get all receipes
   get "/receipes" do
     receipe = Receipe.all
     receipe.to_json
   end
 
-  
+  #Get single receipe
+  get "/receipes/:id" do
+    receipe = Receipe.find(params[:id])
+    receipe.to_json
+  end
+
 
 end
