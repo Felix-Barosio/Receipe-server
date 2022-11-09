42f7d27be9c8bb0deef49feb69d86ceaa68a0f15
Felix <barosiofelix@gmail.com>
2022-11-09 20:22:32 +0300

    Added values to messages tables

--- db/migrate/20221107115712_create_messages.rb
+++ db/migrate/20221107115712_create_messages.rb
@@ -1,4 +1,11 @@
 class CreateMessages < ActiveRecord::Migration[6.1]
   def change
+    create_table :messages do |t|
+      t.string :name
+      t.string :email
+      t.string :message
+      
+      t.timestamps null: false
+    end
   end
 end
