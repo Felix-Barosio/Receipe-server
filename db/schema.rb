7b18c36686dc8f942e0d21b4f76e4d65294682a8
Felix <barosiofelix@gmail.com>
2022-11-10 18:39:27 +0300

    Added messages info to file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_06_062015) do
+ActiveRecord::Schema.define(version: 2022_11_07_115712) do
+
+  create_table "messages", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "message"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "receipes", force: :cascade do |t|
     t.string "meal_name"
